166d45fdccc74d8ba84ab2ff5eca64283f88038a
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2023-09-08 22:27:52 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,14 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=vayu
+device.name2=bhima
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -29,7 +26,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
